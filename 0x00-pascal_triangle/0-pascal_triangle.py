e5558794df2238b608744d875c2685e69c737bba
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-10-01 22:55:14 +0300

    hello

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """
-# End every row with 1
+Pascal's Triangle module
 """
+
 def pascal_triangle(n):
+  
     """
+    Generate Pascal's Triangle up to the nth row.
+    
     Parameters:
     n (int): The number of rows to generate. Must be a positive integer.
     
@@ -18,9 +22,11 @@ def pascal_triangle(n):
 
     for i in range(1, n):
         prev_row = triangle[-1]
+        
         new_row = [1]  # Start every row with 1
 
-        for j in range(1, i): # Fill the middle values of the row
+				# Fill the middle values of the row
+        for j in range(1, i): 
             new_row.append(prev_row[j-1] + prev_row[j])
             
         new_row.append(1)  # End every row with 1
