f696c640e8d713f7576cf3303bd866f383c122a3
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-10-01 22:53:22 +0300

    changes

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-0-pascal_triangle
+# End every row with 1
 """
 def pascal_triangle(n):
     """
@@ -14,16 +14,16 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = [[1]]
+    triangle = [[1]] # Initialize with the first row
 
     for i in range(1, n):
         prev_row = triangle[-1]
-        new_row = [1]  
+        new_row = [1]  # Start every row with 1
 
-        for j in range(1, i):
+        for j in range(1, i): # Fill the middle values of the row
             new_row.append(prev_row[j-1] + prev_row[j])
             
-        new_row.append(1)  
+        new_row.append(1)  # End every row with 1
         
         triangle.append(new_row)
 
