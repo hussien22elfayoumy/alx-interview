63bd8c935d336879374a022274dfe6c173ec7a01
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-11-13 15:42:21 +0200

    check

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,19 +1,54 @@
 #!/usr/bin/node
 
 const request = require('request');
-// prettier-ignore
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, body) {
-  if (err) throw err;
-  const actors = JSON.parse(body).characters;
-  exactOrder(actors, 0);
-});
 
-// prettier-ignore
-const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
+if (process.argv.length !== 3) {
+  console.log('Usage: node script.js <Movie ID>');
+  process.exit(1);
+}
+
+const movieId = process.argv[2];
+const baseUrl = 'https://swapi.dev/api/films/';
+
+request(`${baseUrl}${movieId}/`, (error, response, body) => {
+  if (error) {
+    console.error(`Error fetching data: ${error}`);
+    process.exit(1);
+  }
+
+  if (response.statusCode !== 200) {
+    console.error(`Failed to fetch data: ${response.statusCode}`);
+    process.exit(1);
+  }
+
+  const movieData = JSON.parse(body);
+
+  if (!movieData.title || !movieData.characters) {
+    console.log('Invalid Movie ID or data format error.');
+    process.exit(1);
+  }
+
+  console.log(`Characters in '${movieData.title}':`);
+
+  const characterRequests = movieData.characters.map((url) => {
+    return new Promise((resolve, reject) => {
+      request(url, (error, response, body) => {
+        if (error) {
+          reject(`Error fetching character data: ${error}`);
+        } else if (response.statusCode !== 200) {
+          reject(`Failed to fetch character data: ${response.statusCode}`);
+        } else {
+          resolve(JSON.parse(body).name);
+        }
+      });
+    });
   });
-};
+
+  Promise.all(characterRequests)
+    .then((characterNames) => {
+      characterNames.forEach((name) => console.log(name));
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
