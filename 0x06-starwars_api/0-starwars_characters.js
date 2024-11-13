d1b21003cf31d276a17100343febe5f3c5e88356
hussien22elfayoumy <hussienmedo36@gmail.com>
2024-11-13 15:37:27 +0200

    last fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,15 +2,16 @@
 
 const request = require('request');
 // prettier-ignore
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, body) {
   if (err) throw err;
   const actors = JSON.parse(body).characters;
   exactOrder(actors, 0);
 });
+
 // prettier-ignore
 const exactOrder = (actors, x) => {
   if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
+  request(actors[x], function (err, body) {
     if (err) throw err;
     console.log(JSON.parse(body).name);
     exactOrder(actors, x + 1);
